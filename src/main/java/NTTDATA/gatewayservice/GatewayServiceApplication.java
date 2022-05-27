38c16f6e833c0a38b6d50ab18135d9d01233e763
rbenitec <105337433+rbenitec@users.noreply.github.com>
2022-05-27 12:30:00 -0500

    update

--- src/main/java/NTTDATA/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/NTTDATA/gatewayservice/GatewayServiceApplication.java
@@ -2,7 +2,9 @@ package NTTDATA.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
